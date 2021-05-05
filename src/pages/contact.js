5711e6fe0a74f145aafbaf0eac692a877194a180
Dev-Mehta <devbmehta04@gmail.com>
2021-05-05 15:31:57 +0530

    Reconfigured Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" netlify>
+          <form className="form-container" name="contact" data-netlify="true" method="POST">
             <div>
               <label htmlFor="w3lName">Name</label>
               <input type="text" name="w3lName" id="w3lName"/>
