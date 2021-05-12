ab5719ce9be56f40190adf36153d77536dffdfb4
Dev-Mehta <devbmehta04@gmail.com>
2021-05-12 22:12:20 +0530

    Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,12 +60,12 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    `gatsby-plugin-sass`, 
+	`gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-dark-mode',
     // siteURL is a must for sitemap generation
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
+	`gatsby-plugin-offline`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>&copy; {new Date().getFullYear()} SimplifiedWeb &bull; Crafted with <span role="img" aria-label="tea">☕</span> by <a href="https://github.com/Dev-Mehta/">Dev Mehta</a></p>
       </footer>
     </div>
   )
