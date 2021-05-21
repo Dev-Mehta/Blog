43650f7bccf080b23f8d1b41bea8575b34775805
Dev-Mehta <devbmehta04@gmail.com>
2021-05-21 11:58:10 +0530

    Changed code for whole website

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Navigation from "../components/navigation";
 import "prismjs/themes/prism-okaidia.css";
 import { ThemeConsumer } from "styled-components";
 import themeChanger from "./themeChanger";
+import { Helmet } from "react-helmet";
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -18,23 +19,32 @@ export default ({ children }) => {
     `
   );
   return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
-        </div>
-        <Navigation />
-      </header>
-      {children}
-      <footer className="site-footer">
-        <p>
-          &copy; {new Date().getFullYear()} SimplifiedWeb &bull; Crafted with{" "}
-          <span role="img" aria-label="tea">
-            ☕
-          </span>{" "}
-          by <a href="https://github.com/Dev-Mehta/">Dev Mehta</a>
-        </p>
-      </footer>
-    </div>
+    <>
+      <Helmet>
+        <script
+          data-ad-client="ca-pub-1123574059568760"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      </Helmet>
+      <div className="site-wrapper">
+        <header className="site-header">
+          <div className="site-title">
+            <Link to="/">{data.site.siteMetadata.title}</Link>
+          </div>
+          <Navigation />
+        </header>
+        {children}
+        <footer className="site-footer">
+          <p>
+            &copy; {new Date().getFullYear()} SimplifiedWeb &bull; Crafted with{" "}
+            <span role="img" aria-label="tea">
+              ☕
+            </span>{" "}
+            by <a href="https://github.com/Dev-Mehta/">Dev Mehta</a>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
