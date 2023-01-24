8a0f0dcf3465e7432ea84ebc99574b1ccde7b81b
Dev-Mehta <devbmehta04@gmail.com>
2023-01-24 17:01:24 +0530

    Added AdSense Code and Analytics Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,15 +38,29 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-32B2WMKLZV", // Google Analytics / GA
         ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: false,
           // Setting this parameter is also optional
           respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
         },
       },
     },
@@ -66,6 +80,12 @@ module.exports = {
       resolve: `gatsby-plugin-disqus`,
       options: {
         shortname: `simplifiedweb`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-1123574059568760`
       },
     },
     {
