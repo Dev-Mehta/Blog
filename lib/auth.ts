bf960a2c98b3713f3d8c4753fcea2f0d45e2aac4
Dev-Mehta <devbmehta04@gmail.com>
2023-11-15 00:47:32 +0530

    Change provider to github provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const {
   handlers: { GET, POST },
@@ -7,7 +7,7 @@ export const {
 } = NextAuth({
   trustHost: true,
   providers: [
-    GitHub({
+    GitHubProvider({
       clientId: process.env.OAUTH_CLIENT_KEY as string,
       clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
     }),
